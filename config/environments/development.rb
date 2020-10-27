5e5ad2e53599b0d87db16be14669ee07120588fe
samo2105 <samuel.cea95@gmail.com>
2020-10-27 13:27:00 -0300

    modified i18n

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,8 +49,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = false
-  config.i18n.available_locales = [:'es-CL', :en, :fr]
-  config.i18n.default_locale = :'es-CL'
+  config.i18n.available_locales = [:es, :en, :fr]
+  config.i18n.default_locale = :es
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,8 +52,8 @@ Rails.application.configure do
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
-  config.i18n.available_locales = [:'es-CL', :en, :fr]
-  config.i18n.default_locale = :'es-CL'
+  config.i18n.available_locales = [:es, :en, :fr]
+  config.i18n.default_locale = :es
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
